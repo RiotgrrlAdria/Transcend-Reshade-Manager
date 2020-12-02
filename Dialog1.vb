7f9091564217f56ac09339f7128c256034e7c5a0
Adria Warren <10893349+RiotgrrlAdria@users.noreply.github.com>
2020-12-02 09:52:29 -0600

    Add project files.

---
+++ Dialog1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Windows.Forms
+
+Public Class LicenseDialog
+
+
+    Private Sub Dialog1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        REM Two variables are used as a means of having some false safe way of ensuring that the user has agreed to the terms.
+
+        My.Settings.BoolFirstRun = True
+
+        If My.Settings.BoolFirstRun = True Or My.Settings.boolLicenseAgree = False Then
+
+        Else
+            Form1.Show()
+            Me.Close()
+        End If
+    End Sub
+
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        My.Settings.BoolFirstRun = False
+
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        My.Settings.boolLicenseAgree = True
+        Form1.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        My.Settings.BoolFirstRun = False
+
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Transcend Reshade Manager")>
+<Assembly: AssemblyDescription("Simple and powerful Reshade management software")>
+<Assembly: AssemblyCompany("Transcend Softworks")>
+<Assembly: AssemblyProduct("Transcend Reshade Manager")>
+<Assembly: AssemblyCopyright("Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e1287ee3-5863-4ada-97ee-a8c50f06f477")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("20.11.30.001")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
